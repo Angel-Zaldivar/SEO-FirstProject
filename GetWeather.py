4278c123e2ed0dca56669284b2f681df54126562
Angel Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-07-03 15:39:31 -0700

    main asks for city and shows current weather, deleting humidity from both files

--- GetWeather.py
+++ GetWeather.py
@@ -3,16 +3,15 @@ import json
 
 #API Key: 5956245bd2754f27a65213703240307
 
-def retrieveForecast(city):
+def retrieveWeather(city):
     important_Weather_Details = {}
     response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=5956245bd2754f27a65213703240307&q={city}&days=1&aqi=yes&alerts=yes')
 
     data = response.json()
 
-    important_Weather_Details['Current_Forecast'] = data['current']['condition']['text']
+    important_Weather_Details['Current_Weather'] = data['current']['condition']['text']
     important_Weather_Details['Current_temp_c'] = data['current']['temp_c']
     important_Weather_Details['Current_temp_f'] = data['current']['temp_f']
-    important_Weather_Details['Current_humidity'] = data['current']['humidity']
 
     return important_Weather_Details
 
---
+++ main.py
@@ -0,0 +1,12 @@
+from GetWeather import retrieveWeather
+
+
+print("Want to know the forecast for today, just input a city!")
+city = input('City: ')
+
+print('-'*20)
+data = retrieveWeather(city)
+
+print('The current Weather is: ', data['Current_Weather'])
+print(f"The current temperature in Celsius is: {data['Current_temp_c']}")
+print(f"The current temperature in Fahrenheit is: {data['Current_temp_f']}")
