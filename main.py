5896cd2f22e0f8f3ee9d9c68e6e789b3fae33b8b
Angel Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-07-04 11:23:17 -0700

    Solidifying the options menu

--- main.py
+++ main.py
@@ -6,22 +6,42 @@ print("View Saved Locations(0), or lookup a new location(1)")
 choice = input("Enter your choice: ")
 db = DB()
 
-if choice == "1":
-    city = input('City: ')
-    print('-'*20)
-    data = retrieveWeather(city)
+while True:
+    if choice == "1":
+        city = input('City: ')
+        print('---'*20)
+        data = retrieveWeather(city)
 
-    print(f'The current Weather Condition for {city} is:  {data['Current_Weather']}')
-    print(f"The current temperature in Celsius is: {data['Current_temp_c']}")
-    print(f"The current temperature in Fahrenheit is: {data['Current_temp_f']}")
+        print(f"The current Weather Condition for {city} is:  {data['Current_Weather']}")
+        print(f"The current temperature in Celsius is: {data['Todays_high_c']}")
+        print(f"The current temperature in Fahrenheit is: {data['Todays_high_f']}")
 
-    save_ans = input("Would you like to save this city?(y/n): ")
+        save_ans = input("Would you like to save this city?(y/n): ")
 
-    if save_ans == 'y':
-        db.insert_values(city, data)
-else:
-    #user provides city and weather get returned,
-    #weather should get updated
-    #-------figure out how to update tables info---------
-    print('------Which city do you want up to day info from?------')
-    print('Your current saved cities: ')
+        if save_ans == 'y':
+            db.insert_values(city, data)
+    else:
+        print('------Which city do you want up to day info from?------')
+        print('Your current saved cities: ')
+        cities = db.saved_cities()
+
+        for city in cities:
+            print(city)
+
+        print('---'*20)
+        desired_city = input("Which city would you like to look at?: ")
+
+        same_date = db.compare_data(desired_city)
+
+        if same_date == False:
+            db.update_db(desired_city)
+            db.print_desired_city(desired_city)
+        else:
+            db.print_desired_city(desired_city)
+
+    cont = input("Would you like to continue?(y/n): ")
+    if cont == 'n':
+        break
+    else:
+        print("View Saved Locations(0), or lookup a new location(1)")
+        choice = input("Enter your choice: ")
