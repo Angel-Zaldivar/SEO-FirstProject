ff45c9b98963248123513e8128459f0253c527b7
Angel Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-07-04 11:22:37 -0700

    Changing data values, and adding date value

--- GetWeather.py
+++ GetWeather.py
@@ -1,15 +1,17 @@
+from datetime import date
 import requests
 import json
 
 def retrieveWeather(city):
     important_Weather_Details = {}
-    response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=5956245bd2754f27a65213703240307&q={city}&days=1&aqi=yes&alerts=yes')
+    response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=5956245bd2754f27a65213703240307&q={city}&days=1&aqi=no&alerts=no')
 
     data = response.json()
     important_Weather_Details['City'] = city
     important_Weather_Details['Current_Weather'] = data['current']['condition']['text']
-    important_Weather_Details['Current_temp_c'] = data['current']['temp_c']
-    important_Weather_Details['Current_temp_f'] = data['current']['temp_f']
+    important_Weather_Details['Todays_high_c'] = data['current']['Todays_high_c']
+    important_Weather_Details['Todays_high_f'] = data['current']['Todays_high_f']
+    important_Weather_Details['Todays_Date'] = date.today()
 
     return important_Weather_Details
 
