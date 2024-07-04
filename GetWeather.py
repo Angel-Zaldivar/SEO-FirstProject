9af58a2dcaa36d00d13bd262a32bd5d1dc22f7ef
Angel Zaldivar <ZaldivarAngel.2004@gmail.com>
2024-07-04 09:36:50 -0700

    Added city to data

--- GetWeather.py
+++ GetWeather.py
@@ -6,7 +6,6 @@ def retrieveWeather(city):
     response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=5956245bd2754f27a65213703240307&q={city}&days=1&aqi=yes&alerts=yes')
 
     data = response.json()
-
     important_Weather_Details['City'] = city
     important_Weather_Details['Current_Weather'] = data['current']['condition']['text']
     important_Weather_Details['Current_temp_c'] = data['current']['temp_c']
